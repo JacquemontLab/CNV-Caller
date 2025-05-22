f0a40ce286b2068a2722c413ef30fa19a89fa5c2
Florian Bénitière <flben@narval2.narval.calcul.quebec>
2025-05-21 22:19:54 -0400

    update

--- modules/CNV_calling_per_sampleID/resources/usr/bin/compute_cnv_overlap_fraction.sh
+++ modules/CNV_calling_per_sampleID/resources/usr/bin/compute_cnv_overlap_fraction.sh
@@ -13,7 +13,7 @@
 # Date: April 2025
 ###############################################################################
 
-set -euo pipefail
+# set -euo pipefail
 
 # Check input arguments
 if [ "$#" -ne 3 ]; then
