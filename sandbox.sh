73cebb6b7238da3e653915ab64c5a1922d06122f
fbenitiere <florian.benitiere@gmail.com>
2025-06-12 07:50:51 -0400

    update

--- sandbox.sh
+++ sandbox.sh
@@ -20,12 +20,30 @@ gsutil -m -u $workspace_id cp quantisnp_cnv.tsv.gz ${bucket_id}output_cnvcalling
 gsutil -m -u $workspace_id cp ${bucket_id}output_cnvcalling/cnv_annotation/inputs/quantisnp_cnv.tsv.gz .
 gsutil -m -u $workspace_id cp ${bucket_id}output_cnvcalling/cnv_annotation/inputs/penncnv_cnv.tsv.gz .
 
-gsutil -m -u $workspace_id cp child_cnv_penncnv.bed  ${bucket_id}analysis_mp/
-gsutil -m -u $workspace_id cp parents_cnv_penncnv.bed  ${bucket_id}analysis_mp/
+gsutil -m -u $workspace_id cp child_cnv_quantisnp.bed  ${bucket_id}analysis_mp/
+gsutil -m -u $workspace_id cp parents_cnv_quantisnp.bed  ${bucket_id}analysis_mp/
 
 
-gsutil -m -u $workspace_id cp ${bucket_id}analysis_mp/child_cnv_penncnv.bed .
-gsutil -m -u $workspace_id cp  ${bucket_id}analysis_mp/parents_cnv_penncnv.bed .
+workspace_id="terra-vpc-sc-bf15287e"
+bucket_id="gs://fc-secure-8bde5181-67bc-4770-96db-3b707c3f187f/"
+
+gsutil -m -u $workspace_id cp ${bucket_id}analysis_mp/child_cnv_quantisnp.bed .
+gsutil -m -u $workspace_id cp  ${bucket_id}analysis_mp/parents_cnv_quantisnp.bed .
+
+bedtools intersect -a child_cnv_penncnv.bed -b parents_cnv_penncnv.bed -f 0.1 -r > intersect_penncnv_ovlap0.1.bed
+
+gsutil -m -u $workspace_id cp intersect_quantisnp_ovlap0.1.bed  ${bucket_id}analysis_mp/
+
+
+gsutil -m -u $workspace_id cp  ${bucket_id}analysis_mp/intersect_quantisnp_ovlap0.1.bed  .
+
+
+bedtools intersect -a child_cnv_merged.bed -b parents_cnv_merged.bed -f 0.1 -r > intersect_merged_ovlap0.1.bed
+
+gsutil -m -u $workspace_id cp  ${bucket_id}analysis_mp/child_cnv_merged.bed .
+gsutil -m -u $workspace_id cp  ${bucket_id}analysis_mp/parents_cnv_merged.bed .
+
+gsutil -m -u $workspace_id cp  ${bucket_id}analysis_mp/intersect_merged_ovlap0.1.bed .
 
 
 gsutil -m -u $workspace_id cp -r gs://fc-aou-datasets-controlled/v8/microarray/plink/ .
