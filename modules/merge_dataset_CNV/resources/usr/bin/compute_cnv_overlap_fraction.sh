10698b37bb3d6286c067791c1f13b4eadc44b280
Bénitière Florian <34925751+FloBeni@users.noreply.github.com>
2025-10-07 09:17:33 -0400

    Update compute_cnv_overlap_fraction.sh
    size = $3 - $2 + 1;  # Use +1 if end is inclusive

--- modules/merge_dataset_CNV/resources/usr/bin/compute_cnv_overlap_fraction.sh
+++ modules/merge_dataset_CNV/resources/usr/bin/compute_cnv_overlap_fraction.sh
@@ -102,7 +102,7 @@ for item in ${algorithms_to_overlap//,/ }; do
     # - Calculate the overlap as a fraction of the size
     temp_frac_overlap_bed=$(mktemp)
     awk -F'\t' 'BEGIN{OFS="\t"} {
-            size = $3 - $2;
+            size = $3 - $2 + 1;  # Use +1 if end is inclusive
             overlap = $NF;
             frac = overlap / size ;
             print $0, frac
